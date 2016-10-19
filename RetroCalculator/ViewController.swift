33a9f502ed3c5eb73e4bccbe1b5debbda162ff1b
parasjaved <development@Parass-iMac.local>
2016-10-19 11:16:51 -0400

    Added functions to the buttons and added assets and made a clear button

--- RetroCalculator/ViewController.swift
+++ RetroCalculator/ViewController.swift
@@ -12,13 +12,39 @@ import AVFoundation
 class ViewController: UIViewController {
     
     var buttonSound: AVAudioPlayer!
+    
+    @IBOutlet weak var clearButton: UIButton!
+    
+    
+    
+    enum Operation: String {
+        case Divide = "/"
+        case Multiply = "*"
+        case Subtract = "-"
+        case Add = "+"
+        case Empty = "Empty"
+    }
+    
+    var currentOperation = Operation.Empty
+    
 
     @IBOutlet weak var outputLabel: UILabel!
+    
+    
+    var runningNumber = ""
+    
+    var leftValStr = ""
+    
+    var rightValStr = ""
+    
+    var result = ""
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        outputLabel.text = "0"
         
         let path = Bundle.main.path(forResource: "btn", ofType: "wav")
         let soundUrl = URL(fileURLWithPath: path!)
@@ -26,7 +52,7 @@ class ViewController: UIViewController {
         do {
             try buttonSound = AVAudioPlayer(contentsOf: soundUrl)
             buttonSound.prepareToPlay()
-        } catch is Error {
+        } catch  is Error {
             print(Error.self)
         }
         
@@ -36,6 +62,41 @@ class ViewController: UIViewController {
     
     @IBAction func numberPressed(sender: UIButton) {
         playSound()
+        
+        runningNumber += "\(sender.tag)"
+        outputLabel.text = runningNumber
+    }
+    
+    @IBAction func onDividePressed(sender: AnyObject) {
+        processOperation(operation: .Divide)
+    }
+    
+    @IBAction func onMultiplyPressed(sender: AnyObject) {
+        processOperation(operation: .Multiply)
+    }
+    
+    @IBAction func onSubtractPressed(sender: AnyObject) {
+        processOperation(operation: .Subtract)
+    }
+    
+    @IBAction func onAddPressed(sender: AnyObject) {
+        processOperation(operation: .Add)
+    }
+    
+    @IBAction func onEqualPressed(sender: AnyObject) {
+        processOperation(operation: currentOperation)
+    }
+    
+//    @IBAction func clearButtonPressed(sender: AnyObject) {
+//        outputLabel.text = "0"
+//        runningNumber = "0"
+//        result = ""
+//    }
+    
+    @IBAction func clearTotal(sender: AnyObject) {
+        outputLabel.text = ""
+        result = ""
+        currentOperation = .Empty
     }
     
     func playSound() {
@@ -45,6 +106,39 @@ class ViewController: UIViewController {
         
         buttonSound.play()
     }
+    
+    func processOperation(operation: Operation) {
+        playSound()
+        if currentOperation != Operation.Empty {
+            
+            // Auser selected an operator, but then selected another operator without first entering a number.
+            if runningNumber != "" {
+                rightValStr = runningNumber
+                runningNumber = ""
+                
+                if currentOperation == Operation.Multiply {
+                    result = "\(Double(leftValStr)! * Double(rightValStr)!)"
+                } else if currentOperation == Operation.Divide {
+                    result = "\(Double(leftValStr)! / Double(rightValStr)!)"
+                } else if currentOperation == Operation.Subtract {
+                    result = "\(Double(leftValStr)! - Double(rightValStr)!)"
+                } else if currentOperation == Operation.Add {
+                    result = "\(Double(leftValStr)! + Double(rightValStr)!)"
+                }
+                
+                leftValStr = result
+                outputLabel.text = result
+            }
+            
+            currentOperation = operation
+        } else {
+            // This is the first time an operator has been pressed.
+            leftValStr = runningNumber
+            runningNumber = result
+            currentOperation = operation
+            
+        }
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -53,4 +147,34 @@ class ViewController: UIViewController {
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
